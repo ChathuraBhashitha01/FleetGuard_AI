b11475b1e76728cdc560c7522c9578c96d9983ae
Bethmi Jayamila <bethmi.j@pram-it.com>
2026-03-19 01:33:43 +0530

    Delete Code directory

--- Code/src/services/userService.js
+++
@@ -1,6 +0,0 @@
-import api from '../api/api';
-
-export const userService = {
-  updateProfile: (body) => api.put('/users/me', body).then(r => r.data),
-  getDrivers:    ()     => api.get('/users/drivers').then(r => r.data),
-};
